43515cfc314e0c11f40428d5920cb6d9bbb8ed70
kensho-pilkey <97925854+kensho-pilkey@users.noreply.github.com>
2023-11-19 16:27:45 +0000

    Initial commit

---
+++ code/main.ts
@@ -0,0 +1,29 @@
+import kaboom from "kaboom"
+import "kaboom/global"
+
+// initialize context
+kaboom()
+
+loadSprite("blue", "backgrounds/blue.png");
+
+
+// load assets
+loadSprite("bean", "sprites/bean.png")
+
+// add a character to screen
+let background = add([
+	// list of components
+	sprite("blue"),
+	pos(width() / 2, height() / 2),
+	area(),
+  fixed(),
+  //scale(width(), height())
+  
+])
+
+
+// add a kaboom on mouse click
+onClick(() => {
+	//addKaboom(mousePos())
+})
+
